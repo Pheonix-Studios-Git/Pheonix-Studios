43ff99759c372b7c6f23dab447a13e6308e78740
AkshuDev <akshobhyasasun@gmail.com>
2026-05-13 21:29:01 +0530

    A lot

--- js/footer.js
+++ js/footer.js
@@ -5,11 +5,11 @@ function loadFooter() {
             © 2026, Pheonix Studios / AkshuDev
         </p>
         <ul class="footer-links">
-            <li><a href="../index.html">Home</a></li>
-            <li><a href="cpu.html">CPU</a></li>
-            <li><a href="os.html">OS</a></li>
-            <li><a href="firmware.html">Firmware</a></li>
-            <li><a href="docs.html">Docs</a></li>
+            <li><a href="../index">Home</a></li>
+            <li><a href="cpu">CPU</a></li>
+            <li><a href="os">OS</a></li>
+            <li><a href="firmware">Firmware</a></li>
+            <li><a href="docs">Docs</a></li>
         </ul>
         <div class="footer-socials">
             <a href="https://github.com/Pheonix-Studios-Git" target="_blank">GitHub - Pheonix Studios</a>
